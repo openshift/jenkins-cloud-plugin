1915d2ecf2389f4c9c56e0be308b8720f266e253
Ben Parees <bparees@redhat.com>
2014-04-04 11:23:06 -0400

    allow for optional specification of jar cache path via JENKINS_JAR_CACHE_PATH

--- src/main/java/hudson/plugins/openshift/OpenShiftComputerLauncher.java
+++ src/main/java/hudson/plugins/openshift/OpenShiftComputerLauncher.java
@@ -107,11 +107,16 @@ public class OpenShiftComputerLauncher extends ComputerLauncher {
             String sshWrapperPath = "/usr/libexec/openshift/cartridges/jenkins/bin/git_ssh_wrapper.sh";
             ((ChannelExec) slaveChannel).setEnv("GIT_SSH", sshWrapperPath);
             ((ChannelExec) slaveChannel).setAgentForwarding(true);
-            ((ChannelExec) slaveChannel)
-                    .setCommand("java -jar $OPENSHIFT_DATA_DIR/jenkins/slave.jar");
-            // jar-cache parameter needed for jenkins 1.540+
-            //((ChannelExec) slaveChannel)
-            //       .setCommand("java -jar $OPENSHIFT_DATA_DIR/jenkins/slave.jar -jar-cache $OPENSHIFT_DATA_DIR/.jenkins/cache/jars");
+            String jarCachePath=System.getenv("JENKINS_JAR_CACHE_PATH");
+            if(jarCachePath!=null) {
+                //jar-cache parameter needed for jenkins 1.540+
+                // recommended jar caceh path: $OPENSHIFT_DATA_DIR/.jenkins/cache/jars
+                ((ChannelExec) slaveChannel)
+                .setCommand("java -jar $OPENSHIFT_DATA_DIR/jenkins/slave.jar -jar-cache "+jarCachePath);
+            } else {
+                ((ChannelExec) slaveChannel)
+                        .setCommand("java -jar $OPENSHIFT_DATA_DIR/jenkins/slave.jar");
+            }
             InputStream serverOutput = slaveChannel.getInputStream();
             OutputStream clientInput = slaveChannel.getOutputStream();
             slaveChannel.connect();
